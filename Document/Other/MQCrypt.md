977fb7f498b04049a33b1cd7b41f35a0a8b12788
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2019-08-13 19:56:29 +0800

    add AES128 decrypt

--- Document/Other/MQCrypt.md
+++ Document/Other/MQCrypt.md
@@ -9,15 +9,27 @@
 func encrypt(data strIn: String, key: String, vi: String? = nil) -> String
 ```
 
-对base64字符串进行AES128加密
+base64字符串, AES128加密
+
+```
+func decrypt(data strIn: String, key: String, vi: String? = nil) -> String
+```
+
+base64字符串, AES128解密
 
 ```
 func encrypt(data dataIn: Data, key: Data, vi: Data? = nil) -> Data
 ```
 
-对Data进行AES128加密, 填充模式为`PKCS7Padding`
+Data, AES128加密, 填充模式为`PKCS7Padding`
 
 若存在偏移`vi`不为nil则使用`CBC`模式, 否则使用`ECB`模式
+
+```
+func decrypt(data dataIn: Data, key: Data, vi: Data? = nil) -> Data
+```
+
+Data, AES128解密, 规则同上
 
 # Data+MQCrypt
 
--- MQKit/MQKit/Other/MQCrypt/MQCrypt+AES.swift
+++ MQKit/MQKit/Other/MQCrypt/MQCrypt+AES.swift
@@ -15,6 +15,25 @@ extension MQCrypt {
         
         /// base64 String
         static func encrypt(data strIn: String, key: String, vi: String? = nil) -> String {
+            return self.crypt(op: CCOperation(kCCEncrypt), data: strIn, key: key, vi: vi)
+        }
+        
+        /// base64 String
+        static func decrypt(data strIn: String, key: String, vi: String? = nil) -> String {
+            return self.crypt(op: CCOperation(kCCDecrypt), data: strIn, key: key, vi: vi)
+        }
+        
+        /// ECB: vi == nil, CBC: vi != nil
+        static func encrypt(data dataIn: Data, key: Data, vi: Data? = nil) -> Data {
+            return self.crypt(op: CCOperation(kCCEncrypt), data: dataIn, key: key, vi: vi)
+        }
+        
+        /// ECB: vi == nil, CBC: vi != nil
+        static func decrypt(data dataIn: Data, key: Data, vi: Data? = nil) -> Data {
+            return self.crypt(op: CCOperation(kCCDecrypt), data: dataIn, key: key, vi: vi)
+        }
+        
+        fileprivate static func crypt(op: CCOperation, data strIn: String, key: String, vi: String? = nil) -> String {
             guard let strInData = Data(base64Encoded: strIn) else {
                 return ""
             }
@@ -27,12 +46,11 @@ extension MQCrypt {
                 viData = Data(base64Encoded: nowVi)
             }
             
-            let opt = self.encrypt(data: strInData, key: keyData, vi: viData)
+            let opt = self.crypt(op: op, data: strInData, key: keyData, vi: viData)
             return opt.base64EncodedString()
         }
         
-        /// ECB: vi == nil, CBC: vi != nil
-        static func encrypt(data dataIn: Data, key: Data, vi: Data? = nil) -> Data {
+        fileprivate static func crypt(op: CCOperation, data dataIn: Data, key: Data, vi: Data? = nil) -> Data {
             //使用.withUnsafeBytes { $0.baseAddress } 可能会出现数据异常, 无法还原原始Data, 原因未知
             //因此这里均使用.copyBytes
             let c_dataIn = UnsafeMutablePointer<UInt8>.allocate(capacity: dataIn.count)
@@ -54,7 +72,7 @@ extension MQCrypt {
             let c_dataOut = UnsafeMutablePointer<UInt8>.allocate(capacity: dataOutAvaliable)
             var dataOutMoved = 0
             
-            let result = CCCrypt(CCOperation(kCCEncrypt),
+            let result = CCCrypt(op,
                                  CCAlgorithm(kCCAlgorithmAES),
                                  options,
                                  c_key, kCCBlockSizeAES128, c_vi,
