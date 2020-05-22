ad1cef2b6e5103282076fc0069b77fb70e02d968
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2020-05-23 01:06:41 +0800

    add MTLClearColor+MQ

---
+++ MQKit/MQKit/Extension/Metal/MTLClearColor+MQ.swift
@@ -0,0 +1,72 @@
+//
+//  MTLClearColor+MQ.swift
+//  MQKit
+//
+//  Created by M_Quadra on 2020/5/23.
+//  Copyright © 2020 M_noAria. All rights reserved.
+//
+
+import UIKit
+
+extension MTLClearColor {
+    
+    public static var mq_black: MTLClearColor {
+        return UIColor.black.mq_mtlClearColor
+    }
+    
+    public static var mq_darkGray: MTLClearColor {
+        return UIColor.darkGray.mq_mtlClearColor
+    }
+    
+    public static var mq_lightGray: MTLClearColor {
+        return UIColor.lightGray.mq_mtlClearColor
+    }
+    
+    public static var mq_white: MTLClearColor {
+        return UIColor.white.mq_mtlClearColor
+    }
+    
+    public static var mq_gray: MTLClearColor {
+        return UIColor.gray.mq_mtlClearColor
+    }
+    
+    public static var mq_red: MTLClearColor {
+        return UIColor.red.mq_mtlClearColor
+    }
+    
+    public static var mq_green: MTLClearColor {
+        return UIColor.green.mq_mtlClearColor
+    }
+    
+    public static var mq_blue: MTLClearColor {
+        return UIColor.blue.mq_mtlClearColor
+    }
+    
+    public static var mq_cyan: MTLClearColor {
+        return UIColor.cyan.mq_mtlClearColor
+    }
+    
+    public static var mq_yellow: MTLClearColor {
+        return UIColor.yellow.mq_mtlClearColor
+    }
+    
+    public static var mq_magenta: MTLClearColor {
+        return UIColor.magenta.mq_mtlClearColor
+    }
+    
+    public static var mq_orange: MTLClearColor {
+        return UIColor.orange.mq_mtlClearColor
+    }
+    
+    public static var mq_purple: MTLClearColor {
+        return UIColor.purple.mq_mtlClearColor
+    }
+    
+    public static var mq_brown: MTLClearColor {
+        return UIColor.brown.mq_mtlClearColor
+    }
+    
+    public static var mq_random: MTLClearColor {
+        return UIColor.mq_random.mq_mtlClearColor
+    }
+}
