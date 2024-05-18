5fd4adb2c11ce7156f0718c3a6b846d323144044
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2024-05-19 00:22:37 +0800

    feat: more build func

--- Sources/MQKit/Util/DSL/UITextField+DSL.swift
+++ Sources/MQKit/Util/DSL/UITextField+DSL.swift
@@ -10,152 +10,164 @@ import UIKit
 public extension UIViewDSL where T: UITextField {
     
     @discardableResult
-    func text(_ text: String?) -> Self {
+    consuming func text(_ text: String?) -> Self {
         self.object.text = text
         return self
     }
-
+    
     @discardableResult
-    func attributedText(_ attributedText: NSAttributedString?) -> Self {
+    consuming func attributedText(_ attributedText: NSAttributedString?) -> Self {
         self.object.attributedText = attributedText
         return self
     }
-
+    
     @discardableResult
-    func textColor(_ textColor: UIColor?) -> Self {
+    consuming func textColor(_ textColor: UIColor?) -> Self {
         self.object.textColor = textColor
         return self
     }
-
+    
     @discardableResult
-    func font(_ font: UIFont?) -> Self {
+    consuming func font(_ font: UIFont?) -> Self {
         self.object.font = font
         return self
     }
-
+    
     @discardableResult
-    func textAlignment(_ textAlignment: NSTextAlignment) -> Self {
+    consuming func textAlignment(_ textAlignment: NSTextAlignment) -> Self {
         self.object.textAlignment = textAlignment
         return self
     }
-
+    
     @discardableResult
-    func borderStyle(_ borderStyle: UITextField.BorderStyle) -> Self {
+    consuming func borderStyle(_ borderStyle: UITextField.BorderStyle) -> Self {
         self.object.borderStyle = borderStyle
         return self
     }
-
+    
     @discardableResult
-    func defaultTextAttributes(_ defaultTextAttributes: [NSAttributedString.Key : Any]) -> Self {
+    consuming func defaultTextAttributes(_ defaultTextAttributes: [NSAttributedString.Key : Any]) -> Self {
         self.object.defaultTextAttributes = defaultTextAttributes
         return self
     }
-
+    
     @discardableResult
-    func placeholder(_ placeholder: String?) -> Self {
+    consuming func placeholder(_ placeholder: String?) -> Self {
         self.object.placeholder = placeholder
         return self
     }
-
+    
     @discardableResult
-    func attributedPlaceholder(_ attributedPlaceholder: NSAttributedString?) -> Self {
+    consuming func attributedPlaceholder(_ attributedPlaceholder: NSAttributedString?) -> Self {
         self.object.attributedPlaceholder = attributedPlaceholder
         return self
     }
-
+    
     @discardableResult
-    func clearsOnBeginEditing(_ clearsOnBeginEditing: Bool) -> Self {
+    consuming func clearsOnBeginEditing(_ clearsOnBeginEditing: Bool) -> Self {
         self.object.clearsOnBeginEditing = clearsOnBeginEditing
         return self
     }
-
+    
     @discardableResult
-    func adjustsFontSizeToFitWidth(_ adjustsFontSizeToFitWidth: Bool) -> Self {
+    consuming func adjustsFontSizeToFitWidth(_ adjustsFontSizeToFitWidth: Bool) -> Self {
         self.object.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth
         return self
     }
-
+    
     @discardableResult
-    func minimumFontSize(_ minimumFontSize: CGFloat) -> Self {
+    consuming func minimumFontSize(_ minimumFontSize: CGFloat) -> Self {
         self.object.minimumFontSize = minimumFontSize
         return self
     }
-
+    
     @discardableResult
-    func delegate(_ delegate: UITextFieldDelegate?) -> Self {
+    consuming func delegate(_ delegate: UITextFieldDelegate?) -> Self {
         self.object.delegate = delegate
         return self
     }
-
+    
     @discardableResult
-    func background(_ background: UIImage?) -> Self {
+    consuming func background(_ background: UIImage?) -> Self {
         self.object.background = background
         return self
     }
-
+    
     @discardableResult
-    func disabledBackground(_ disabledBackground: UIImage?) -> Self {
+    consuming func disabledBackground(_ disabledBackground: UIImage?) -> Self {
         self.object.disabledBackground = disabledBackground
         return self
     }
-
+    
     @discardableResult
-    func allowsEditingTextAttributes(_ allowsEditingTextAttributes: Bool) -> Self {
+    consuming func allowsEditingTextAttributes(_ allowsEditingTextAttributes: Bool) -> Self {
         self.object.allowsEditingTextAttributes = allowsEditingTextAttributes
         return self
     }
-
+    
     @discardableResult
-    func typingAttributes(_ typingAttributes: [NSAttributedString.Key : Any]?) -> Self {
+    consuming func typingAttributes(_ typingAttributes: [NSAttributedString.Key : Any]?) -> Self {
         self.object.typingAttributes = typingAttributes
         return self
     }
-
+    
     @discardableResult
-    func clearButtonMode(_ clearButtonMode: UITextField.ViewMode) -> Self {
+    consuming func clearButtonMode(_ clearButtonMode: UITextField.ViewMode) -> Self {
         self.object.clearButtonMode = clearButtonMode
         return self
     }
-
+    
     @discardableResult
-    func leftView(_ leftView: UIView?) -> Self {
+    consuming func leftView(_ leftView: UIView?) -> Self {
         self.object.leftView = leftView
         return self
     }
-
+    
     @discardableResult
-    func leftViewMode(_ leftViewMode: UITextField.ViewMode) -> Self {
+    consuming func leftViewMode(_ leftViewMode: UITextField.ViewMode) -> Self {
         self.object.leftViewMode = leftViewMode
         return self
     }
-
+    
     @discardableResult
-    func rightView(_ rightView: UIView?) -> Self {
+    consuming func rightView(_ rightView: UIView?) -> Self {
         self.object.rightView = rightView
         return self
     }
-
+    
     @discardableResult
-    func rightViewMode(_ rightViewMode: UITextField.ViewMode) -> Self {
+    consuming func rightViewMode(_ rightViewMode: UITextField.ViewMode) -> Self {
         self.object.rightViewMode = rightViewMode
         return self
     }
-
+    
     @discardableResult
-    func inputView(_ inputView: UIView?) -> Self {
+    consuming func inputView(_ inputView: UIView?) -> Self {
         self.object.inputView = inputView
         return self
     }
-
+    
     @discardableResult
-    func inputAccessoryView(_ inputAccessoryView: UIView?) -> Self {
+    consuming func inputAccessoryView(_ inputAccessoryView: UIView?) -> Self {
         self.object.inputAccessoryView = inputAccessoryView
         return self
     }
-
+    
     @discardableResult
-    func clearsOnInsertion(_ clearsOnInsertion: Bool) -> Self {
+    consuming func clearsOnInsertion(_ clearsOnInsertion: Bool) -> Self {
         self.object.clearsOnInsertion = clearsOnInsertion
+        return self
+    }
+    
+    @discardableResult
+    consuming func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControl.Event = .editingChanged) -> Self {
+        self.object.addTarget(target, action: action, for: controlEvents)
+        return self
+    }
+    
+    @discardableResult
+    consuming func returnKeyType(_ returnKeyType: UIReturnKeyType) -> Self {
+        self.object.returnKeyType = returnKeyType
         return self
     }
 }
