18b2ddce0b20026b3a955bcb7fe1e40f71ec0ca9
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2024-09-19 00:12:34 +0800

    feat: remove deprecated func

--- Sources/MQKit/Util/Crypto/Hash/Hash+MD5.swift
+++ Sources/MQKit/Util/Crypto/Hash/Hash+MD5.swift
@@ -12,20 +12,7 @@ import CommonCrypto
 
 public extension Crypto {
     
-    static func md5(_ data: Data) -> Data {
-        if #available(iOS 13.0, *) {
-            let opt = Insecure.MD5.hash(data: data)
-            return Data(opt)
-        }
-        
-        return data.withUnsafeBytes { ptr in
-            guard let dataPtr = ptr.baseAddress else { return Data() }
-            
-            let length = Int(CC_MD5_DIGEST_LENGTH)
-            let optPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: length)
-            
-            CC_MD5(dataPtr, CC_LONG(data.count), optPtr)
-            return Data(bytesNoCopy: optPtr, count: length, deallocator: .free)
-        }
+    static func md5(_ data: consuming Data) -> Data {
+        return Data(Insecure.MD5.hash(data: data))
     }
 }
