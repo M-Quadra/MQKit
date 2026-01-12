0392ca1c17db9d1eea2c5e7d46c73491eebf8c52
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2026-01-13 00:36:14 +0800

    feat: UITableView dequeue

--- Sources/MQKit/Extension/UIKit/UITableView+MQ.swift
+++ Sources/MQKit/Extension/UIKit/UITableView+MQ.swift
@@ -16,13 +16,23 @@ public extension UITableView {
             self.register(cell, forCellReuseIdentifier: consume identifier)
         }
     }
+    
+    func register(headerFooter: UITableViewHeaderFooterView.Type) {
+        let identifier = NSStringFromClass(headerFooter)
+        self.register(headerFooter, forHeaderFooterViewReuseIdentifier: consume identifier)
+    }
 }
 
 // MARK: - Dequeue
 public extension UITableView {
     
-    func dequeue<T: UITableViewCell>(cell: T.Type = T.self, for indexPath: IndexPath) -> T {
+    func dequeueCell<T: UITableViewCell>(_ cell: T.Type = T.self, for indexPath: IndexPath) -> T {
         let identifier = NSStringFromClass(cell)
         return self.dequeueReusableCell(withIdentifier: consume identifier, for: indexPath) as! T
+    }
+    
+    func dequeueHeaderFooter<T: UITableViewHeaderFooterView>(_ headerFooter: T.Type = T.self) -> T {
+        let identifier = NSStringFromClass(headerFooter)
+        return self.dequeueReusableHeaderFooterView(withIdentifier: consume identifier) as! T
     }
 }
